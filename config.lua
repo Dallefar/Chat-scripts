e8b00b9b37296785acff00654817a4a12e6e11de
Dallefar <94932668+math32d1@users.noreply.github.com>
2024-02-16 01:36:46 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,9 @@
 config = {}
 
+-- ooc -- 
+
+config.useooc = false -- Jeg har lavet dette fordi på vrp server er der alleerede en command og hvis du ikke har fjernt den anden så kommer ooc to gange
+
 -- Webhooks -- 
 
 config.Twitter = "https://discord.com/api/webhooks/1200471967359303872/Hx-4FyaYC0c0w-q4WNXA9HhFlSSDALq72bhl_BAaG1pCUo1VA7rVHbQeo6qto0i5pgBs"
